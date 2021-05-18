3dfe0c1ce05e9e88397ef9b67526149599796ce7
nepodeveloper <neponed@gmail.com>
2021-05-18 11:02:43 +0200

    WebApp connected to API

--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
 import { PaymentDetail } from './payment-detail.model';
+import {HttpClient} from "@angular/common/http"
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  readonly baseURL = 'http://localhost:39029/api/PaymentDetail'
   formData: PaymentDetail = new PaymentDetail();
+  list : PaymentDetail [];
+
+  postPaymentDetail(){
+   return this.http.post(this.baseURL,this.formData);
+  }
+
+  refreshList(){
+    this.http.get(this.baseURL)
+    .toPromise()
+    .then(res => this.list = res as PaymentDetail[]);
+  }
 }
